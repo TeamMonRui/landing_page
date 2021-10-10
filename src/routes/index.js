bc0336b3de0486b809a6999ca9941d6941b574fa
Team MonRui <team.development.monrui@gmail.com>
2021-10-09 20:58:12 -0500

    Se quita titulo de rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,16 @@ var router = express.Router();
 const emailController = require('../controllers/email.controller')
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Montoya and Jhon', state: -1, rou: 'm'});
+  res.render('index', { state: -1, rou: 'm'});
 });
 
 router.post('/send', async function (req, res) {
   if(req.body.name && req.body.email && req.body.phone && req.body.comment){
     const response = await emailController.send(req.body.name, req.body.email, req.body.phone, req.body.comment)
     if (response === 0){
-      res.render('index', { title: 'Montoya and Jhon', state: 0, rou: 's'})
+      res.render('index', { state: 0, rou: 's'})
     } else {
-      res.render('index', { title: 'Montoya and Jhon', state: -1, rou: 's'})
+      res.render('index', { state: -1, rou: 's'})
     }
   }else{
     res.redirect('/')
